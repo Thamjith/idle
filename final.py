fe0b7d667c9c4dedfb8f119f4f945f15039a8fac
Thamjith <jithuthaha8@gmail.com>
2020-03-25 21:20:00 +0530

    matplotlib added

--- final.py
+++ final.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as graph
 maxIdleTime = 10 #enter the idle time in minutes
 #maxIdleTime*=60 #If you comment this line you will get seconds
 shiftStartTime = datetime.datetime.now()
-shiftTimeEnd = datetime.datetime.strptime('22:00:00', '%H:%M:%S').time() #In 24 hour format
+shiftEndTime = datetime.datetime.strptime('21:12:00', '%H:%M:%S').time() #In 24 hour format
 monitor = IdleMonitor.get_monitor()
 graphDataX = []
 graphDataY = []
@@ -18,9 +18,7 @@ f= open("%s.txt"%(filename),"w+")
 graph.show()
 while True:
 	print(monitor.get_idle_time())
-	# if datetime.datetime.now().time() >= shiftTimeEnd:
-	# 	break
-	if datetime.datetime.now() >= shiftStartTime + datetime.timedelta(minutes = 1):
+	if datetime.datetime.now().time() >= shiftEndTime:
 		break
 	flag1 = 1
 	flag2 = 0
@@ -28,6 +26,8 @@ while True:
 	idleStartTime = datetime.datetime.now().time()
 	while monitor.get_idle_time() > 1:
 		print("---" + str(monitor.get_idle_time()))
+		if datetime.datetime.now().time() >= shiftEndTime:
+			break
 		if monitor.get_idle_time() >= maxIdleTime:
 			if monitor.get_idle_time() > totalIdledTime:
 				totalIdledTime = monitor.get_idle_time()
@@ -38,17 +38,18 @@ while True:
 				# notification for linux
 				Notify.init("App Name")
 				Notify.Notification.new("You have been Idling for %s minutes" %(maxIdleTime)).show()
-
-				# graph.plot(graphDataX, graphDataY)
-				# graph.xlabel('idle')
-				# graph.ylabel('time')
-				# graph.title('Users Idle time') 
-				# graph.show()
 	if flag2:
 		graphDataX.append(datetime.datetime.now())
 		graphDataY.append(totalIdledTime)
-		f.write("	The user has idled for %s minutes\n" % (totalIdledTime))
+		f.write("	The user has idled for %s seconds\n" % (totalIdledTime))
 		f.write("%s " % (graphDataX))
 		f.write("%s \n---------------------------------------\n" % (graphDataY))
 		Notify.uninit()
 f.close
+graph.plot(graphDataX, graphDataY)
+graph.xlabel('time')
+graph.ylabel('idle')
+graph.title('Users Idle time') 
+graph.savefig('graph.png')
+print("Your shift is over")
+# graph.show()
